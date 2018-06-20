ed8c2348b93dcc567a9d495d4fcdbd425ece58dd
Sainfuw <sainfuw@aol.com>
2018-06-19 21:35:00 -0400

    ADD: ex1

--- ejercicio1.rb
+++ ejercicio1.rb
@@ -11,7 +11,7 @@ def parrafos2(dato1, dato2, arreglo)
   file = File.open('index2.html', 'w')
   file.puts("<p>#{dato1}</p>")
   file.puts("<p>#{dato2}</p>")
-  unless arreglo.nil?
+  unless arreglo.empty?
     file.puts('<ol>')
     file.puts(arreglo.map { |i| " <li>#{i}</li>" })
     file.puts('</ol>')
@@ -24,7 +24,7 @@ def parrafos3(dato1, dato2, arreglo, bgcolor)
   file = File.open('index3.html', 'w')
   file.puts("<p style='background-color:#{bgcolor};'>#{dato1}</p>")
   file.puts("<p style='background-color:#{bgcolor};'>#{dato2}</p>")
-  unless arreglo.nil?
+  unless arreglo.empty?
     file.puts('<ol>')
     file.puts(arreglo.map { |i| " <li>#{i}</li>" })
     file.puts('</ol>')
@@ -36,3 +36,9 @@ arr = %w[test de arreglo en archivo]
 parrafos('Hola Bienvenido', 'Emilio')
 parrafos2('Hola Bienvenido', 'Emilio', arr)
 parrafos3('Hola Bienvenido', 'Emilio', arr, 'yellow')
+retorno = parrafos3('Hola Bienvenido', 'Emilio', arr, 'yellow')
+if retorno.nil?
+  puts 'Retorna nulo'
+else
+  puts 'Retorna algo'
+end
